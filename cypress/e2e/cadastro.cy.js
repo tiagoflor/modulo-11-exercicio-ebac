9d89aa1a693d5dba1f6ae6b3c3b893e3f549742a
Tiago Flor <tiagoxxsilva@hotmail.com>
2022-08-29 18:01:28 -0300

    segundo commit exercicio modulo 11

--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -1,16 +1,50 @@
+const { faker } = require('@faker-js/faker')
 
+describe('Criar Cadastro com Email e Senha', () => {
+    
+    const  nomeFaker = faker.name.firstName() // nome fake randomico
+    const  sobreNomeFaker = faker.name.lastName() // sobrenome fake randomico
+    const emailFaker = faker.internet.email(nomeFaker); // email fake randomico e passando parametro do nome
 
-describe('Acesso tela de login', () => {
     beforeEach(() => {
       cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
     });
-    afterEach(() => {
-      cy.screenshot() //evidenciar cada cenário concluído
+   afterEach(() => {
+     cy.screenshot() //evidenciar cada cenário concluído
     });
 
-    it('Criar Cadastro com Email e Senha', () => {
-        
+    //Caminho Feliz
+    it('Criar Pre Cadastro com Sucesso', () => {
+      cy.get('#reg_email').type(emailFaker)
+      cy.get('#reg_password').type('senha@2022')
+      cy.get(':nth-child(4) > .button').click()
+
+      cy.get('.woocommerce-MyAccount-content > :nth-child(3)').should('contain', 'A partir do painel de controle de sua conta, ')
+    });
+
+    it('Completar o pré cadastro com sucesso', () => {
+      cy.get('#reg_email').type(emailFaker)
+      cy.get('#reg_password').type('!teste@teste2')
+      cy.get(':nth-child(4) > .button').click()
+      
+      cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+      cy.get('#account_first_name').type(nomeFaker) 
+      cy.get('#account_last_name').type(sobreNomeFaker) 
+      cy.get('.woocommerce-Button').click()
+
+      cy.get('.woocommerce-message').should('contain', 'Detalhes da conta modificados com sucesso.')
+    });
+
+    //Fluxo Alternativo
+    it('Utilizando e-mail existente', () => {
+      cy.get('#reg_email').type('Jaylan_Pfeffer@gmail.com')
+      cy.get('#reg_password').type('senha@2022')
+      cy.get('.woocommerce-form > .button').click()
+
+      cy.get('.woocommerce-error').should('contain', 'Erro: Uma conta já está registrada com seu endereço de e-mail. Faça login.')
+
     });
 
 
-}
+
+})
